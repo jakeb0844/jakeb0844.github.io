fa95fcd38b35c10619d88aaeb01122f29089d1dc
Jake Busby <jakeb0844@gmail.com>
2025-08-24 11:23:30 -0500

    add custom deploy workflow

--- svelte.config.js
+++ svelte.config.js
@@ -2,16 +2,10 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html'
-		})
-	}
+  kit: {
+    adapter: adapter({ fallback: '404.html' }),
+    paths: { base: '' } // user site => no base path
+  }
 };
 
 export default config;
