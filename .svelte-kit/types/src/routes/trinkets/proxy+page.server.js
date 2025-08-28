80ff07a10a7af4ac33133432b7214874fe227ff3
Jake Busby <jakeb0844@gmail.com>
2025-08-28 09:55:38 -0500

    Updated tier list data.

--- .svelte-kit/types/src/routes/trinkets/proxy+page.server.js
+++ .svelte-kit/types/src/routes/trinkets/proxy+page.server.js
@@ -1,6 +1,6 @@
 // @ts-nocheck
 import { json } from '@sveltejs/kit';
-import trinketData from '../../../data/trinket-tier-list-2025-08-23T17-56-29-855Z.json';
+import trinketData from '../../../data/trinket-tier-list-2025-08-28T14-50-56-717Z.json';
 
 /** */
 export async function load() {
--- src/routes/trinkets/+page.server.js
+++ src/routes/trinkets/+page.server.js
@@ -1,5 +1,5 @@
 import { json } from '@sveltejs/kit';
-import trinketData from '../../../data/trinket-tier-list-2025-08-23T17-56-29-855Z.json';
+import trinketData from '../../../data/trinket-tier-list-2025-08-28T14-50-56-717Z.json';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
